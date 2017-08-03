fad30c82e1a2dbdd9128a20464f7b7d7da2edda0
Peter Carbonetto <peter.carbonetto@gmail.com>
2017-08-03 16:23:40 -0500

    Revised normmix.R analysis to make model different from data simulation.

--- analysis/normmix.R
+++ analysis/normmix.R
@@ -1,15 +1,26 @@
 # TO DO: Give overview of this analysis, and instructions for
 # experimenting with different analysis parameters.
 
-# ANALYSIS PARAMETERS
-# -------------------
-seed <- 1     # Random number generator seed.
-n    <- 5000  # Number of data samples.
+# DATA SIMULATION SETTINGS
+# ------------------------
+# Random number generator seed.
+seed <- 1
 
-# The standard deviations and mixture weights used to simulate the
-# data. 
-s <- c(0,   0.1, 0.2, 0.5)
-w <- c(0.95,0.03,0.01,0.01)
+# Number of data samples.
+n <- 1e4
+
+# The standard deviations and mixture weights used to simulate the data. 
+sim <- list(s = c(0,   0.1, 0.2, 0.5),
+            w = c(0.95,0.03,0.01,0.01))
+
+# MODEL PARAMETERS
+# ----------------
+# The standard deviations of the normal mixture components. 
+s <- c(0.01,10^(seq(-2,0,length.out = 39)))
+
+# OPTIMIZATION SETTINGS
+# ---------------------
+# TO DO.
 
 # LOAD PACKAGES AND FUNCTIONS
 # ---------------------------
@@ -26,18 +37,17 @@ set.seed(1)
 
 # Simulate a data set with n samples, in which the standard errors are ...
 cat(sprintf("Simulating data set with %d observations.\n",n))
-k  <- length(w)
 se <- rep(0.1,n)
-x  <- datasim.norm(w,s,se)
+x  <- datasim.norm(sim$w,sim$s,se)
 
 # COMPUTE LIKELIHOOD MATRIX
 # -------------------------
-cat(sprintf("Computing the %d x %d conditional likelihood matrix.\n",
-            n,length(w)))
+k <- length(s)
+cat(sprintf("Computing the %d x %d conditional likelihood matrix.\n",n,k))
 L <- condlikmatrix.norm(x,se,s)
 
 # FIT MIXTURE MODEL
 # -----------------
 cat("Fitting model to data.\n")
-fit.em <- mixopt.em(L)
-
+out <- system.time(fit.em <- mixopt.em(L))
+cat(sprintf("Model fitting took %0.2f seconds.\n",out["elapsed"]))
