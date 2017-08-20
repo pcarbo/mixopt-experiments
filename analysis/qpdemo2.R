08e0cf87641a915084d0ee589bdd8a50812787ed
Peter Carbonetto <peter.carbonetto@gmail.com>
2017-08-20 10:02:21 -0500

    Added callback functions for qpdemo2.R.

--- analysis/qpdemo2.R
+++ analysis/qpdemo2.R
@@ -5,7 +5,11 @@
 #   * This is a quadratic program with an equality constraint and an
 #     inequality constraint.
 #
-#   * See problem #14 from H&S test problems.
+#   * See problem #14 (QQR-T1-4) from H&S test problems.
+#
+#   * The minimium occurs at x1 = (sqrt(7) - 1)/2, x2 = (sqrt(7) + 1)/4.
+#
+#   * Starting point must be (primal) feasible.
 #
 
 # Load the packages and function definitions used in this demo.
@@ -15,17 +19,32 @@ source("../code/ipsolver.R")
 
 # DEFINE PROBLEM
 # --------------
-# TO DO.
+# This function returns the objective function at x.
+obj <- function (x)
+  (x[1] - 2)^2 + (x[2] - 1)^2
 
+# This function returns the gradient and Hessian of the objective
+# function at x.
+grad <- function (x)
+  list(g = 2*(x[1] - 2,x[2] - 1),
+       H = diag(c(2,2)))
+
+# This function returns the inequality constraint function.
+constr <- function (x)
+  x[1]^2/4 + x[2]^2 - 1
+
+# This function computes the m x n Jacobian of the inequality
+# constraint function, and the the n x n Hessian of the Lagrangian
+# (minus the Hessian of the objective).
+jac <- function (x, z)
+  list(J = t(matrix(c(0.5*x[1],2*x[2]))),
+       W = z*diag(0.5,2))
+    
 # SOLVE PROBLEM USING IP METHOD
 # -----------------------------
 # Solve the quadratic program using the primal-dual interior-point
 # solver.
-out <- ipsolver(x      = rep(0,4),
-                obj    = function (x) quadf(x,H,u),
-                grad   = function (x) list(g = drop(H %*% x + u),H = H),
-                constr = function (x) sapply(constraints,
-                                        function (a) with(a,quadf(x,P,r,-b))),
-                jac    = function (x, z) qpjacobian(x,z,constraints))
+out <- ipsolver(x = c(0,0),obj = obj,grad = grad,constr = constr,jac = jac,
+                A = t(matrix(c(1,-2))),b = -1)
 cat("Solution:\n")
 print(out$x)
